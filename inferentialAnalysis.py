9c5bd1d3fc52c4e8a0655436c7b159cf70e3df69
dnszafir <danielle.szafir@gmail.com>
2018-11-12 12:12:18 -0700

    data and skeleton code

---
+++ inferentialAnalysis.py
@@ -0,0 +1,14 @@
+import numpy as np
+import pandas as pd
+import scipy
+
+import statsmodels
+import statsmodels.api as sm
+from statsmodels.formula.api import ols
+
+# Extract the data. Return both the raw data and dataframe
+def generateDataset(filename):
+    data = pd.read_csv(filename)
+    df = data[0:]
+    df = df.dropna()
+    return data, df
